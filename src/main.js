d8e74a07f7d8e58bc6a98888b7199c5727c14050
Ethan Liang <ehliang@uwaterloo.ca>
2016-05-12 10:30:17 -0400

    sass dependencies and intro popup added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,46 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 
+
+var Main = React.createClass({
+	render: function()
+	{
+		return (
+			<IntroScreen/>
+			);
+	}
+}); 
+
+
+
+var IntroScreen = React.createClass({
+	getInitialState: function() {
+			return {continueState: false}; 
+		}, 
+
+		handleClick: function(event) { 
+			this.setState({continueState: true})
+		}, 
+
+	render: function(){
+
+
+		return (
+
+			<div className="introFloat">
+				<p className="introFloatTitle"  onClick={this.handleClick}>Hey, I'm Ethan Liang {this.state.continueState}</p>
+
+
+			</div> 
+
+
+
+			); 
+	}
+
+}); 
+
 ReactDom.render(
-	<h1>Hello</h1>, 
+	<Main/>, 
 	document.getElementById('main')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
+var path = require('path');
 module.exports = {
   context: __dirname + "/src",
   entry: "./main.js",
 
   output: {
-    filename: "bundle.js",
-    path: __dirname + "/build",
+
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/assets/",
+    filename: "bundle.js"
   },
   module: {
     loaders: [
@@ -15,6 +18,34 @@ module.exports = {
         query: {
           presets: ['react']
         }
+      }, 
+      { 
+        test: /\.css$/, 
+        loader: "style-loader!css-loader" 
+      },
+      { 
+        test: /\.png$/, 
+        loader: "url-loader?limit=100000" 
+      },
+      { 
+        test: /\.jpg$/, 
+        loader: "file-loader" 
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'file'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
       }
     ]
   },
